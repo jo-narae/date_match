f5b18099643a8e9cee329fec9e4ee39a8010c39f
jo-narae <nare6004@gmail.com>
2022-03-08 11:13:19 +0900

    init

---
+++ src/main/java/com/example/dating/DatingApplication.java
@@ -0,0 +1,13 @@
+package com.example.dating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dating/DatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
